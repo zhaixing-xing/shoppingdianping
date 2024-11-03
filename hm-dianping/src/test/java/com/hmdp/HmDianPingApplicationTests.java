bb5bf541bcbf34b482be1c416f18caa6459fcffe
nikaijie <s06091518>
2024-11-03 09:37:10 +0800

    实现秒杀券的全局唯一ID以及测试

--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -2,6 +2,7 @@ package com.hmdp;
 
 import com.hmdp.entity.Shop;
 import com.hmdp.service.IShopService;
+import com.hmdp.utils.RedisIdWorker;
 import org.apache.ibatis.io.DefaultVFS;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +15,9 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 import static com.hmdp.utils.RedisConstants.SHOP_GEO_KEY;
@@ -24,32 +28,8 @@ class HmDianPingApplicationTests {
     private IShopService shopService;
     @Resource
     private StringRedisTemplate stringRedisTemplate;
-    //@Test
-//    void loadShopData() {
-//        // 1.查询店铺信息
-//
-//        List<Shop> list = shopService.list();
-//        // 2.把店铺分组，按照typeId分组，typeId一致的放到一个集合
-//        Map<Long, List<Shop>> map = list.stream().collect(Collectors.groupingBy(Shop::getTypeId));
-//        // 3.分批完成写入Redis
-//        for (Map.Entry<Long, List<Shop>> entry : map.entrySet()) {
-//            // 3.1.获取类型id
-//            Long typeId = entry.getKey();
-//            String key = SHOP_GEO_KEY + typeId;
-//            // 3.2.获取同类型的店铺的集合
-//            List<Shop> value = entry.getValue();
-//            List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(value.size());
-//            // 3.3.写入redis GEOADD key 经度 纬度 member
-//            for (Shop shop : value) {
-//                // stringRedisTemplate.opsForGeo().add(key, new (shop.getX(), shop.getY()), shop.getId().toString());
-//                locations.add(new RedisGeoCommands.GeoLocation<>(
-//                        shop.getId().toString(),
-//                        new (shop.getX(), shop.getY())
-//                ));
-//            }
-//            stringRedisTemplate.opsForGeo().add(key, locations);
-//        }
-//    }
+    @Resource
+    private RedisIdWorker redisIdWorker;
     @Test
     void testHyperLogLog(){
         String[] values = new String[1000];
@@ -65,5 +45,26 @@ class HmDianPingApplicationTests {
         //统计数量
         Long res = stringRedisTemplate.opsForHyperLogLog().size("hl3");
         System.out.println("hl3的数目"+res);
+    }
+
+    private ExecutorService es= Executors.newFixedThreadPool(500);
+    @Test
+    void testIdWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id = " + id);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
     }
 }
